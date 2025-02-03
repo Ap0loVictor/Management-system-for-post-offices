ce342262f28d6645ec9147f47225fe97d9a2bdec
EuclidesLuis <felipeeuclides258@gmail.com>
2025-02-02 21:27:02 -0300

    Deu certo???

--- teste.c
+++ teste.c
@@ -2,6 +2,8 @@
 #include <stdlib.h> 
 #include <string.h> 
 //\n
+// disfbdfbinddifbifndfifinfiddidi
+
 
 // Teste de commit pra ver como funciona
 // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
