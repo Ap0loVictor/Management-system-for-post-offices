4debea6d8b9e8df01506cd7790cca5d4ee40fd1c
Apolo <apolovictorj.c@gmail.com>
2025-02-04 10:06:06 -0300

    Protótipo da tela (Não é função ainda)

--- teste.c
+++ teste.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+// AVISO: Por favor, olhar as alterações dos colegas do grupo
+// Caso tenha algo chato do C busque comentar ex: usar "strcmp" pra saber se strings são iguais
+
 struct Veiculos
 {
     int veiculo_ID;
@@ -32,7 +35,7 @@ struct Clientes
     char tipoServico[20];
 };
 
-void infoVeiculo(struct Veiculos veiculo)
+void cadastrarVeiculo(struct Veiculos veiculo)
 {
     FILE *arquivoVeiculos = fopen("Veiculos.txt", "a");
     if (arquivoVeiculos == NULL)
@@ -44,26 +47,10 @@ void infoVeiculo(struct Veiculos veiculo)
     fprintf(arquivoVeiculos, "ID do Veículo: %d\nTipo: %s\nCarga: %.2f Kg\nStatus: %s\n",
             veiculo.veiculo_ID, veiculo.tipoVeiculo, veiculo.cargaVeiculo, veiculo.statusVeiculo);
     fclose(arquivoVeiculos);
-    printf("\nAs informações do veículo foram salvas no sistema!\n");
+    printf("\nAs informacoes do veiculo foram salvas no sistema!\n");
 }
 
-void dadosVeiculo(struct Veiculos *veiculo)
-{
-    printf("Informe os dados do veículo:\n\n");
-    printf("ID do Veículo: ");
-    scanf("%d", &veiculo->veiculo_ID);
-
-    printf("Tipo de veículo: ");
-    scanf("%s", veiculo->tipoVeiculo);
-
-    printf("Carga máxima (em kg): ");
-    scanf("%f", &veiculo->cargaVeiculo);
-
-    printf("Status (disponível/ocupado): ");
-    scanf("%s", veiculo->statusVeiculo);
-}
-
-void infoEntrega(struct Entregas entrega)
+void cadastrarEntrega(struct Entregas entrega)
 {
     FILE *arquivoEntregas = fopen("Entregas.txt", "a");
     if (arquivoEntregas == NULL)
@@ -75,10 +62,56 @@ void infoEntrega(struct Entregas entrega)
     fprintf(arquivoEntregas, "ID da Entrega: %d\nOrigem: %s\nDestino: %s\nTempo Estimado: %.1f Horas\n",
             entrega.entrega_ID, entrega.origem, entrega.destino, entrega.tempoEstimado);
     fclose(arquivoEntregas);
-    printf("\nAs informações da entrega foram salvas no sistema!\n");
+    printf("\nAs informacoes da entrega foram salvas no sistema!\n");
 }
 
-void dadosEntrega(struct Entregas *entrega)
+void cadastrarFuncionario(struct Funcionarios funcionario)
+{
+    FILE *arquivoFuncionarios = fopen("Funcionarios.txt", "a");
+    if (arquivoFuncionarios == NULL)
+    {
+        printf("Erro ao abrir arquivo.");
+        system("Pause");
+        exit(1);
+    }
+    fprintf(arquivoFuncionarios, "ID do Funcionário: %d\nNome: %s\n",
+            funcionario.funcionario_ID, funcionario.nomeFuncionario);
+    fclose(arquivoFuncionarios);
+    printf("\nAs informacoes do funcionario foram salvas no sistema!\n");
+}
+
+void cadastrarCliente(struct Clientes cliente)
+{
+    FILE *arquivoClientes = fopen("Clientes.txt", "a");
+    if (arquivoClientes == NULL)
+    {
+        printf("Erro ao abrir arquivo.");
+        system("Pause");
+        exit(1);
+    }
+    fprintf(arquivoClientes, "ID do Cliente: %d\nNome: %s\nEndereço: %s\nTipo de Serviço: %s\n",
+            cliente.cliente_ID, cliente.nomeCliente, cliente.endereco, cliente.tipoServico);
+    fclose(arquivoClientes);
+    printf("\nAs informacoes do cliente foram salvas no sistema!\n");
+}
+
+void adicionarVeiculo(struct Veiculos *veiculo)
+{
+    printf("Informe os dados do veiculo:\n\n");
+    printf("ID do Veiculo: ");
+    scanf("%d", &veiculo->veiculo_ID);
+
+    printf("Tipo de veiculo: ");
+    scanf("%s", veiculo->tipoVeiculo);
+
+    printf("Carga maxima (em kg): ");
+    scanf("%f", &veiculo->cargaVeiculo);
+
+    printf("Status (disponivel/ocupado): ");
+    scanf("%s", veiculo->statusVeiculo);
+}
+
+void adicionarEntrega(struct Entregas *entrega)
 {
     printf("\nInforme os dados da entrega:\n");
     printf("ID da entrega: ");
@@ -94,47 +127,17 @@ void dadosEntrega(struct Entregas *entrega)
     scanf("%f", &entrega->tempoEstimado);
 }
 
-void infoFuncionario(struct Funcionarios funcionario)
+void adicionarFuncionario(struct Funcionarios *funcionario)
 {
-    FILE *arquivoFuncionarios = fopen("Funcionarios.txt", "a");
-    if (arquivoFuncionarios == NULL)
-    {
-        printf("Erro ao abrir arquivo.");
-        system("Pause");
-        exit(1);
-    }
-    fprintf(arquivoFuncionarios, "ID do Funcionário: %d\nNome: %s\n",
-            funcionario.funcionario_ID, funcionario.nomeFuncionario);
-    fclose(arquivoFuncionarios);
-    printf("\nAs informações do funcionário foram salvas no sistema!\n");
-}
-
-void dadosFuncionario(struct Funcionarios *funcionario)
-{
-    printf("\nInforme os dados do funcionário:\n");
-    printf("ID do funcionário: ");
+    printf("\nInforme os dados do funcionario:\n");
+    printf("ID do funcionario: ");
     scanf("%d", &funcionario->funcionario_ID);
 
     printf("Nome: ");
     scanf("%s", funcionario->nomeFuncionario);
 }
 
-void infoCliente(struct Clientes cliente)
-{
-    FILE *arquivoClientes = fopen("Clientes.txt", "a");
-    if (arquivoClientes == NULL)
-    {
-        printf("Erro ao abrir arquivo.");
-        system("Pause");
-        exit(1);
-    }
-    fprintf(arquivoClientes, "ID do Cliente: %d\nNome: %s\nEndereço: %s\nTipo de Serviço: %s\n",
-            cliente.cliente_ID, cliente.nomeCliente, cliente.endereco, cliente.tipoServico);
-    fclose(arquivoClientes);
-    printf("\nAs informações do cliente foram salvas no sistema!\n");
-}
-
-void dadosCliente(struct Clientes *cliente)
+void adicionarCliente(struct Clientes *cliente)
 {
     printf("\nInforme os dados do cliente:\n");
     printf("ID do cliente: ");
@@ -143,7 +146,7 @@ void dadosCliente(struct Clientes *cliente)
     printf("Nome: ");
     scanf("%s", cliente->nomeCliente);
 
-    printf("Endereço: ");
+    printf("Endereco: ");
     scanf("%s", cliente->endereco);
 
     printf("Tipo de serviço: ");
@@ -157,17 +160,44 @@ int main()
     struct Funcionarios funcionario;
     struct Clientes cliente;
 
-    dadosVeiculo(&veiculo);
-    infoVeiculo(veiculo);
+    char escolha1[12];
+    char escolha2[13];
+    printf("O que deseja fazer? (Adicionar/Visualizar/Editar/Deletar)\n");
+    scanf("%11s", escolha1);
 
-    dadosEntrega(&entrega);
-    infoEntrega(entrega);
-
-    dadosFuncionario(&funcionario);
-    infoFuncionario(funcionario);
-
-    dadosCliente(&cliente);
-    infoCliente(cliente);
+    if (strcmp(escolha1, "Adicionar") == 0) //Tem q ser assim pra comparar string
+    {
+        printf("O que deseja adicionar? (Veiculo/Entrega/Funcionario/Cliente)\n");
+        scanf("%12s", escolha2);
+        if (strcmp(escolha2, "Veiculo") == 0)
+        {
+            adicionarVeiculo(&veiculo);
+            cadastrarVeiculo(veiculo);
+        }
+        else if (strcmp(escolha2, "Entrega") == 0)
+        {
+            adicionarEntrega(&entrega);
+            cadastrarEntrega(entrega);
+        }
+        else if (strcmp(escolha2, "Funcionario") == 0)
+        {
+            adicionarFuncionario(&funcionario);
+            cadastrarFuncionario(funcionario);
+        }
+        else if (strcmp(escolha2, "Cliente") == 0)
+        {
+            adicionarCliente(&cliente);
+            cadastrarCliente(cliente);
+        }
+        else
+        {
+            printf("Ops! Parece que houve um erro de digitacao");
+        }
+    }
+    else
+    {
+        printf("Ops! Parece que houve um erro de digitcao");
+    }
 
     return 0;
 }
